a149b33c9eb164df17caea93b21dc70b8e07c02f
oBokyo <thanathorn.pl@kkumail.com>
2025-10-04 01:46:16 +0700

    ddd

---
+++ 3d-project-completed222/mob/robot_01.gd
@@ -0,0 +1,43 @@
+extends RigidBody3D
+
+signal died
+
+var speed = randf_range(2.0, 4.0)
+var health = 3
+
+@onready var robot_01 = %robot01
+@onready var timer = %Timer
+
+@onready var player = get_node("/root/Game/Player")
+
+
+
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	direction.y = 0.0
+	linear_velocity = direction * speed
+	robot_01.rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP) + PI
+
+func take_damage():
+	if health == 0:
+		return
+		
+	robot_01.hurt()
+	
+	health -= 1
+	
+	if health == 0:
+
+		set_physics_process(false)
+		gravity_scale = 1.0
+		var direction = player.global_position.direction_to(global_position)
+		var random_upward_force = Vector3.UP * randf() * 5.0
+		apply_central_impulse(direction.rotated(Vector3.UP, randf_range(-0.2, 0.2)) * 10.0 + random_upward_force)
+
+		timer.start()
+
+
+func _on_timer_timeout():
+	queue_free()
+	died.emit()
